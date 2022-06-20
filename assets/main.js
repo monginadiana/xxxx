1f2345188b8dd1d501a3b1ce4c79b0b567f5d48b
monginadiana <dianahmongina2@gmail.com>
2022-06-20 20:48:05 +0000

    addition of the js files

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,3 @@
 function myFunction() {
-  document.getElementById("asign").innerHTML= "Hello       Diana, welcome to the bootcamp";
+document.getElementById("asign").innerHTML= "Hello Diana, welcome to the bootcamp";
 }
